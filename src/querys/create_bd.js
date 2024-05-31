3505110eb3df77f362800598917af734b981f169
Ryan <ryanaugustoo71@hotmail.com>
2024-05-31 14:13:35 -0300

    ../create_bd.js

--- src/querys/create_bd.js
+++ src/querys/create_bd.js
@@ -8,49 +8,52 @@ async function createDataBase(){
         CREATE TYPE SESSION_TYPE AS ENUM('Race', 'Practice', 'Sprint', 'Qualifying')
         `);
 
-        // Create tables
         await db.query(`
         CREATE TABLE drivers (
-            driver_key INTEGER PRIMARY KEY,
+            driver_key BIGINT PRIMARY KEY,
             country_code VARCHAR(3) NOT NULL,
             driver_number INTEGER NOT NULL,
             full_name VARCHAR(50) NOT NULL,
             name_acronym VARCHAR(3) NOT NULL,
             session_key INTEGER NOT NULL,
             team_colour VARCHAR NOT NULL,
-            team_name VARCHAR NOT NULL
+            team_name VARCHAR NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
         await db.query(`
         CREATE TABLE intervals (
-            id INTEGER PRIMARY KEY,
+            id BIGINT PRIMARY KEY,
             date DATE NOT NULL,
             driver_key INTEGER NOT NULL,
             gap_to_leader INTERVAL NOT NULL,
             interval INTEGER NOT NULL,
-            session_key INTEGER NOT NULL
+            session_key INTEGER NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
         await db.query(`
         CREATE TABLE pit_stop (
-            id INTEGER PRIMARY KEY,
+            id BIGINT PRIMARY KEY,
             date DATE NOT NULL,
             driver_key INTEGER NOT NULL,
             lap_number INTEGER NOT NULL,
             pit_duration INTEGER NOT NULL,
-            session_key INTEGER NOT NULL
+            session_key INTEGER NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
         await db.query(`
         CREATE TABLE position (
-            id INTEGER PRIMARY KEY,
+            id BIGINT PRIMARY KEY,
             date DATE NOT NULL,
             driver_key INTEGER NOT NULL,
             position INTEGER NOT NULL,
-            session_key INTEGER NOT NULL
+            session_key INTEGER NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
@@ -66,33 +69,35 @@ async function createDataBase(){
             date_start TIMESTAMPTZ NOT NULL,
             gmt_offset INTERVAL NOT NULL,
             location VARCHAR NOT NULL,
-            year INTEGER NOT NULL
+            year INTEGER NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
         await db.query(`
         CREATE TABLE radio (
-            id INTEGER PRIMARY KEY,
+            id BIGINT PRIMARY KEY,
             date DATE NOT NULL,
             driver_key INTEGER NOT NULL,
             recording_url TEXT NOT NULL,
-            session_key INTEGER NOT NULL
+            session_key INTEGER NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
         await db.query(`
         CREATE TABLE weather (
-            id INTEGER PRIMARY KEY,
+            id BIGINT PRIMARY KEY,
             date DATE NOT NULL,
             session_key INTEGER NOT NULL,
             humidity INTEGER NOT NULL,
             rainfall INTEGER NOT NULL,
             track_temperature NUMERIC NOT NULL,
-            wind_speed NUMERIC NOT NULL
+            wind_speed NUMERIC NOT NULL,
+            deleted_in DATE DEFAULT NULL
         );
         `);
 
-        // Add foreign keys
         await db.query(`
         ALTER TABLE drivers ADD FOREIGN KEY (session_key) REFERENCES sessions (session_key);
         `);
@@ -135,7 +140,7 @@ async function createDataBase(){
         
         await db.end();
         
-        console.log('Database created successfully');
+        console.log('Banco de dados criado com sucesso!');
     }catch(e){
         console.error('Houve um erro ao tentar criar o banco de dados!', e);
     }
