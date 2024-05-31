653dc4b008230ff674c097a03062acadcc76f675
Ryan <ryanaugustoo71@hotmail.com>
2024-05-31 14:13:51 -0300

    ../drop_db.js

--- src/querys/drop_db.js
+++ src/querys/drop_db.js
@@ -4,7 +4,6 @@ async function dropDataBase(){
     try {
         await db.connect();
     
-        // Drop foreign key constraints
         await db.query(`
             ALTER TABLE intervals DROP CONSTRAINT IF EXISTS intervals_driver_key_fkey;
             ALTER TABLE intervals DROP CONSTRAINT IF EXISTS intervals_session_key_fkey;
@@ -33,7 +32,6 @@ async function dropDataBase(){
             ALTER TABLE drivers DROP CONSTRAINT IF EXISTS drivers_session_key_fkey;
         `);
     
-        // Drop tables
         await db.query(`DROP TABLE IF EXISTS sessions`);
         await db.query(`DROP TABLE IF EXISTS radio`);
         await db.query(`DROP TABLE IF EXISTS weather`);
@@ -42,12 +40,11 @@ async function dropDataBase(){
         await db.query(`DROP TABLE IF EXISTS position`);
         await db.query(`DROP TABLE IF EXISTS drivers`);
     
-        // Drop enum type
         await db.query(`DROP TYPE IF EXISTS SESSION_TYPE`);
     
         await db.end();
     
-        console.log('Todas as tabelas e relações foram removidas do banco de dados com sucesso');
+        console.log('Todas as tabelas foram removidas do banco de dados com sucesso');
     } catch (e) {
         console.error('Houve um erro ao tentar remover todas as tabelas e relações do banco de dados:', e);
     }
